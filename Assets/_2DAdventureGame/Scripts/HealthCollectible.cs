a1df02fc8d3be3e84ae9db5ae5e67e8ed218b564
Thomas Juranich <tjuranich777@gmail.com>
2026-04-15 22:16:17 -0700

    2 units left?

---
+++ Assets/_2DAdventureGame/Scripts/HealthCollectible.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class HealthCollectible : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+
+        if (controller != null)
+        {
+            if (controller.health < controller.maxHealth)
+            {
+                controller.ChangeHealth(1);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
--- Assets/_2DAdventureGame/Scripts/PlayerController.cs
+++ Assets/_2DAdventureGame/Scripts/PlayerController.cs
@@ -6,21 +6,64 @@ public class PlayerController : MonoBehaviour
 {
     
     public InputAction MoveAction;
-    public float speed;
+    public float speed = 6.0f;
+    Rigidbody2D rigidbody2d;
+    Vector2 move;
+    public int maxHealth = 5;
+    [SerializeField]
+    private int currentHealth;
+
+    //invin
+    public float timeInvincible = 2.0f;
+    bool isInvincible;
+    float damageCooldown;
+
+
+    public void ChangeHealth(int amount)
+    {
+        if (amount < 0)
+        {
+            if (isInvincible)
+            {
+                return;
+            }
+            isInvincible = true;
+            damageCooldown = timeInvincible;
+        }
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
+    }
+    public int health { get { return currentHealth; } }
+
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
-        transform.position = position;
+        move = MoveAction.ReadValue<Vector2>();
+        //Debug.Log(move);
+        if (isInvincible)
+        {
+            damageCooldown -= Time.deltaTime;
+            if (damageCooldown < 0)
+            {
+                isInvincible = false;
+            }
+        }
     }
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.deltaTime;
+        rigidbody2d.MovePosition(position);
+    }
+   
 }
